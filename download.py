3d01700abb1fc58b1c9a43776151697434ac2aa4
Glen Cornell <glen.m.cornell@gmail.com>
2023-06-11 08:03:19 -0400

    Update download.py

--- download.py
+++ download.py
@@ -3,30 +3,43 @@ import zipfile
 import csv
 import psycopg2
 
-# constants
-url = 'https://232app.azurewebsites.net/data/BIS232Data.zip'
-tempDir = './temp/'
-zipFileName = tempDir + 'BIS232Data.zip'
 
-# Step 1: Download the zip archive
-response = requests.get(url)
-with open(zipFileName, "wb") as zip_file:
-    zip_file.write(response.content)
+class section232Updater:
+    def __init__(self):
+        # constants
+        self.url = 'https://232app.azurewebsites.net/data/BIS232Data.zip'
+        self.tempDir = './temp/' # TODO: use mkstemp() instead
+        self.zipFileName = self.tempDir + 'BIS232Data.zip'
 
-# Step 2: Extract the zip archive
-with zipfile.ZipFile(tempDir + zipFileName, "r") as zip_ref:
-    zip_ref.extractall(tempDir)
+    def downloadZipFile(self):
+        # Step 1: Download the zip archive
+        response = requests.get(self.url)
+        with open(self.zipFileName, "wb") as zip_file:
+            zip_file.write(response.content)
 
-# Step 3: Process each CSV file
-# TODO: replace `file1.csv` with files in zip archive
-csv_files = ["file1.csv", "file2.csv", "file3.csv"]  # Update with the actual file names
-for csv_file in csv_files:
-    with open(f"extracted_folder/{csv_file}", "r") as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)  # Skip header if present
-        for row in csv_reader:
-            # Step 4: Update records in the database
-            # Parse the row data and execute necessary queries using psycopg2
-            # Example: connection.execute("UPDATE table_name SET ... WHERE ...")
+    def extractZipFile(self):
+        # Step 2: Extract the zip archive
+        with zipfile.ZipFile(self.zipFileName, "r") as zip_ref:
+            zip_ref.extractall(tempDir + "extracted")
 
-# Clean up - delete the downloaded zip and extracted files if needed
+    def updateDatabase(self):
+        # Step 3: Process each CSV file
+        # TODO: replace `file1.csv` with files in zip archive
+        csv_file = "file1.csv" 
+        with open(tempDir + f"extracted/{csv_file}", "r") as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)  # Skip header if present
+            for row in csv_reader:
+                # Step 4: Update records in the database
+                # Parse the row data and execute necessary queries using psycopg2
+                # Example: connection.execute("UPDATE table_name SET ... WHERE ...")
+
+    def cleanup(self):
+        # Clean up - delete the downloaded zip and extracted files if needed
+        # TODO: remove tempDir
+    
+if __name__ == "__main__":
+    Section232Updater updater
+    updater = downloadZipFile()
+    cleanup()
+ 
